f4ef3e2b68b282eb8d8bf3e6053265d94ba8099c
steinsut <51322187+steinsut@users.noreply.github.com>
2026-04-08 07:54:31 +0300

    Initial commit

---
+++ src/main/java/me/steinsut/entropylib/EntropyLib.java
@@ -0,0 +1,28 @@
+package me.steinsut.entropylib;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import org.slf4j.Logger;
+
+@Mod(EntropyLib.MODID)
+public class EntropyLib {
+    public static final String MODID = "entropylib";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public EntropyLib(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("Overengineering in progress...");
+    }
+}
---
+++ src/main/java/me/steinsut/entropylib/EntropyLibClient.java
@@ -0,0 +1,24 @@
+package me.steinsut.entropylib;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = EntropyLib.MODID, dist = Dist.CLIENT)
+
+@EventBusSubscriber(modid = EntropyLib.MODID, value = Dist.CLIENT)
+public class EntropyLibClient {
+    public EntropyLibClient(ModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        EntropyLib.LOGGER.info("Client overengineering in progress...");
+    }
+}
