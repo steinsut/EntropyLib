4cd4a63ac60a6917a068edf50a299ab8db1b0aed
steinsut <51322187+steinsut@users.noreply.github.com>
2026-04-09 06:46:40 +0300

    chore: change dyn renderer registry identifiers

--- src/main/java/me/steinsut/entropylib/api/registries/Registries.java
+++ src/main/java/me/steinsut/entropylib/api/registries/Registries.java
@@ -13,11 +13,11 @@ public class Registries {
     private static final int DYN_RENDERER_TYPE_MAX_ID = 255;
 
     public static final ResourceKey<Registry<DynRendererDataType<?, ?>>> DYN_RENDERER_DATA_TYPE_REGISTRY_KEY = ResourceKey.createRegistryKey(
-            Identifier.fromNamespaceAndPath(EntropyLibApi.MOD_ID, "renderer_data_types")
+            Identifier.fromNamespaceAndPath(EntropyLibApi.MOD_ID, "dyn_data_types")
     );
 
     public static final ResourceKey<Registry<BaseDynRendererType<?, ?, ?>>> DYN_RENDERER_TYPE_REGISTRY_KEY = ResourceKey.createRegistryKey(
-            Identifier.fromNamespaceAndPath(EntropyLibApi.MOD_ID, "renderer_type")
+            Identifier.fromNamespaceAndPath(EntropyLibApi.MOD_ID, "dyn_type")
     );
 
     public static final Registry<DynRendererDataType<?, ?>> DYN_RENDERER_DATA_TYPE_REGISTRY = new RegistryBuilder<>(DYN_RENDERER_DATA_TYPE_REGISTRY_KEY)
