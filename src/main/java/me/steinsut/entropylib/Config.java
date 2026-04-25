9fc4e8f5b1f7387b5f6663c9be43014a9975ca86
steinsut <51322187+steinsut@users.noreply.github.com>
2026-04-25 16:28:31 +0300

    chore: add LGPL-3.0 license

--- src/main/java/me/steinsut/entropylib/Config.java
+++ src/main/java/me/steinsut/entropylib/Config.java
@@ -1,9 +1,19 @@
+/*
+Copyright © 2026 steinsut
+
+This file is part of EntropyLib.
+
+EntropyLib is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+EntropyLib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with EntropyLib. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package me.steinsut.entropylib;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Neo's config APIs
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
